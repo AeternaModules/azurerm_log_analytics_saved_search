0de6fab96a2fb494880a36364c2ab24bfa9f9fda
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "log_analytics_saved_searches" {
-  description = "All log_analytics_saved_search resources"
-  value       = azurerm_log_analytics_saved_search.log_analytics_saved_searches
-}
 output "log_analytics_saved_searches_category" {
-  description = "List of category values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.category]
+  description = "Map of category values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.category }
 }
 output "log_analytics_saved_searches_display_name" {
-  description = "List of display_name values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.display_name]
+  description = "Map of display_name values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.display_name }
 }
 output "log_analytics_saved_searches_function_alias" {
-  description = "List of function_alias values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.function_alias]
+  description = "Map of function_alias values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.function_alias }
 }
 output "log_analytics_saved_searches_function_parameters" {
-  description = "List of function_parameters values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.function_parameters]
+  description = "Map of function_parameters values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.function_parameters }
 }
 output "log_analytics_saved_searches_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.log_analytics_workspace_id }
 }
 output "log_analytics_saved_searches_name" {
-  description = "List of name values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.name]
+  description = "Map of name values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.name }
 }
 output "log_analytics_saved_searches_query" {
-  description = "List of query values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.query]
+  description = "Map of query values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.query }
 }
 output "log_analytics_saved_searches_tags" {
-  description = "List of tags values across all log_analytics_saved_searches"
-  value       = [for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : v.tags]
+  description = "Map of tags values across all log_analytics_saved_searches, keyed the same as var.log_analytics_saved_searches"
+  value       = { for k, v in azurerm_log_analytics_saved_search.log_analytics_saved_searches : k => v.tags }
 }
 
